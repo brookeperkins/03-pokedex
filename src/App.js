9542e9458ab5a6bc8d19ffce59c339581f38451e
brookeperkins <brookemmperkins@gmail.com>
2020-08-06 08:36:58 -0700

    eslint

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import './App.css';
-import Header from './Header.js';
-import request from 'superagent';
+// import Header from './Header.js';
+// import request from 'superagent';
 
-class App extends React.Component {
+export default class App extends React.Component {
   state = {
     search: '',
     isLoading: false,
     pokeState: []
   }
 
-  handleClick = async () => {
-    this.setState({ isLoading: true })
-    const data = await 
-  }
+  // handleClick = async () => {
+  //   this.setState({ isLoading: true })
+  //   const data = await 
+  // }
 
-
-  return (
-    <>
-    <body>
-      <Header />
-
-    </body>
-    </>
-  );
+  // return (
+  //     <body>
+  //       <Header />
+  //     </body>
+  //   );
 }
-
-export default App;
