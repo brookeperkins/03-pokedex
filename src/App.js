e1993b9835adaf1c9136606d569ae2cf7a7edb46
brookeperkins <brookemmperkins@gmail.com>
2020-08-06 08:33:11 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js';
+import request from 'superagent';
 
-function App() {
+class App extends React.Component {
+  state = {
+    search: '',
+    isLoading: false,
+    pokeState: []
+  }
+
+  handleClick = async () => {
+    this.setState({ isLoading: true })
+    const data = await 
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <body>
+      <Header />
+
+    </body>
+    </>
   );
 }
 
